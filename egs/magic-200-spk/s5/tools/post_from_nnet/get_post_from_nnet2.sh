8f652591b5dd18b354c6d47e51015c968fe29c29
Jinyi Yang <jyang126@jhu.edu>
2018-07-05 14:44:15 -0400

    Get post from nnet2

--- egs/magic-200-spk/s5/tools/post_from_nnet/get_post_from_nnet2.sh
+++ egs/magic-200-spk/s5/tools/post_from_nnet/get_post_from_nnet2.sh
@@ -4,10 +4,10 @@
 . ./utils/parse_options.sh
 . cmd.sh
 
-mdldir=exp/nnet5c
-srcdata=data_babble_noise/test_eval92_noise_babble_snr
+mdldir=exp/nnet5c_gpu
+srcdata=data/dev_20spk
 pdf_phone_map=nnet5c_phone_mapping/pdf_to_pseudo_phone.txt  # pdf-id to phone-id map
-postdir=post/nnet5c/nnet_test_eval92_noise_babble
+postdir=post/nnet5c_gpu/dev_20spk
 feat_type="lda"
 apply_log="false"
 no_softmax="false"
@@ -40,10 +40,14 @@ else
 	mdl=$mdldir/final.mdl
 fi
 
+#$cmd JOB=1:$nj $p_dir/log/nnet_am_compute.JOB.log \
+#nnet-am-compute --apply-log="$apply_log" $mdl "$feats" ark:- \| \
+#transform-nnet-posteriors --pdf-to-pseudo-phone=$pdf_phone_map \
+#ark:- ark,t:$p_dir/monophone.JOB.post
+
 $cmd JOB=1:$nj $p_dir/log/nnet_am_compute.JOB.log \
 nnet-am-compute --apply-log="$apply_log" $mdl "$feats" ark:- \| \
-transform-nnet-posteriors --pdf-to-pseudo-phone=$pdf_phone_map \
-ark:- ark,t:$p_dir/monophone.JOB.post
+post-to-phone-post $mdl ark:- ark,t:$p_dir/monophone.JOB.post
 
 for i in `seq 1 $nj`;do
 (    sed -i 's/^[ \t]*//' $p_dir/monophone.$i.post
