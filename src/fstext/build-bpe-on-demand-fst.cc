09b060c56e2ff85ce7de88fd014799727df381f2
Jinyi Yang <jyang126@jhu.edu>
2019-08-14 15:30:03 -0400

    Add test scripts for BPE OnDemandFst

--- src/fstext/build-bpe-on-demand-fst.cc
+++ src/fstext/build-bpe-on-demand-fst.cc
@@ -1,3 +1,5 @@
+// fstext/build-bpe-on-demand-fst.cc
+
 #include "base/kaldi-common.h"
 #include "util/common-utils.h"
 #include "lat/kaldi-lattice.h"
@@ -32,8 +34,6 @@ WordLexiconInfo::WordLexiconInfo(const std::vector<std::vector<int32> >&lexicon)
     }
 }
 
-
-
 void WordLexiconInfo::UpdateLexiconMap(const std::vector<int32> &lexicon_entry){
     KALDI_ASSERT(lexicon_entry.size() >= 2);
     std::vector<int32> key;
@@ -53,24 +53,39 @@ void WordLexiconInfo::UpdateLexiconMap(const std::vector<int32> &lexicon_entry){
 } //namespace
 int main(int argc, char *argv[]) {
     try{
+     const char *usage =
+        "Convert BPE lattice to word lattice, by building a BPE OnDemandFst\n"
+				"with BPE lattice and lexicon, then compose this FST with given lattice.\n"
+        "Usage: build-bpe-on-demand-fst [options] <lexicon> \\\n"
+				"<lattice-rspecifier> <lattice-wspecifier>\n"
+        " e.g.: build-bpe-on-demand-fst "
+        "    data/local/dict_bpe/lexicon.int ark:in.lats ark:out.lats \\\n"
         using namespace kaldi;
         using kaldi::int32;
         const char *usage =
         "Test reading the lexicon file and latstream";
     ParseOptions po(usage);
     po.Read(argc, argv);
+    if (po.NumArgs() != 3) {
+      po.PrintUsage();
+      exit(1);
+    }
     std::string lexicon_rxfilename = po.GetArg(1);
+    std::string lats_rspecifier = po.GetArg(2);
+    std::string lats_wspecifier = po.GetArg(3);
     std::vector<std::vector<int32> > lexicon;
-    {
-        bool binary_in;
-        Input ki(lexicon_rxfilename, &binary_in);
-        KALDI_ASSERT(!binary_in && "Not expecting binary file for lexicon");
-        if (!ReadLexiconForWordAlign(ki.Stream(), &lexicon)){
-           KALDI_ERR << "Error reading alignment lexicon from "
-                     << lexicon_rxfilename;
-        }
+    bool binary_in;
+
+    // Read lexicon and store in map
+    Input ki(lexicon_rxfilename, &binary_in);
+    KALDI_ASSERT(!binary_in && "Not expecting binary file for lexicon");
+    if (!ReadLexiconForWordAlign(ki.Stream(), &lexicon)){
+       KALDI_ERR << "Error reading alignment lexicon from "
+                 << lexicon_rxfilename;
     }
     WordLexiconInfo lexicon_info(lexicon);
+    SequentialCompactLatticeReader compact_lattice_reader(lats_rspecifier);
+    CompactLatticeWrite compact_lattice_write(lats_wspecifier);
     //lexicon_info.PrintLexicon();
     { std::vector<std::vector<int32> > temp; lexicon.swap(temp); }
     } catch(const std::exception &e) {
