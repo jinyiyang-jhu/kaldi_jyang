eefff0962ff6e4bb4fdb24be3e8566b5fa81fa66
Jinyi Yang <jyang126@jhu.edu>
2019-11-18 19:20:35 -0500

    Use srilm to train LM instead of Kaldi lm (faster)

--- egs/gale_tdt_mandarin/s5/local/train_lms.sh
+++ egs/gale_tdt_mandarin/s5/local/train_lms.sh
@@ -7,82 +7,58 @@ oov_sym="<UNK>"
 . path.sh
 . utils/parse_options.sh
 
-if [ $# != 2 ]; then
-  echo "Usage: <lm-src-dir> <lm-dir>"
-  echo "E.g. $0 --ngram-order 4 <data/local/train> <data/local/lm_no_extra>"
+if [ $# != 4 ]; then
+  echo "Usage: <lm-src-dir> <dict-dir> <lm-dir> <heldout>"
+  echo "E.g. $0 --ngram-order 4 data/local/train data/local/dict
+  data/local/lm_no_extra datal/local/dev/text"
+  exit 1
 fi
 
 text=$1/text
-dir=$2
+dict_dir=$2
+dir=$3
+heldout=$4
 
-[ ! -d $dir ] && mkdir -p $dir
+[ ! -d $dir ] && mkdir -p $dir && exit 1;
 [ ! -f $text ] && echo "$0: No such file $text" && exit 1;
 
-lexicon=data/local/dict/lexicon.txt
+lexicon=$dict_dir/lexicon.txt
 [ ! -f $lexicon ] && echo "$0: No such file $lexicon" && exit 1;
 
-# check if sri is installed or no
-sri_installed=false
-which ngram-count  &>/dev/null
-if [[ $? == 0 ]]; then
-sri_installed=true
-fi
 
-# This script takes no arguments.  It assumes you have already run
-# previus steps successfully
-# It takes as input the files
-#data/local/train.*/text
-#data/local/dict/lexicon.txt
+cleantext=$dir/text.no_oov
 
-
-export LC_ALL=C # You'll get errors about things being not sorted, if you
-# have a different locale.
-export PATH=$PATH:$KALDI_ROOT/tools/kaldi_lm
-( # First make sure the kaldi_lm toolkit is installed.
- cd $KALDI_ROOT/tools || exit 1;
- if [ -d kaldi_lm ]; then
-   echo Not installing the kaldi_lm toolkit since it is already there.
- else
-   echo Downloading and installing the kaldi_lm tools
-   if [ ! -f kaldi_lm.tar.gz ]; then
-     wget http://www.danielpovey.com/files/kaldi/kaldi_lm.tar.gz || exit 1;
-   fi
-   tar -xvzf kaldi_lm.tar.gz || exit 1;
-   cd kaldi_lm
-   make || exit 1;
-   echo Done making the kaldi_lm tools
- fi
-) || exit 1;
-
- cleantext=$dir/text.no_oov
-
- cat $text | awk -v lex=$lexicon 'BEGIN{while((getline<lex) >0){ seen[$1]=1; } }
+cat $text | awk -v lex=$lexicon 'BEGIN{while((getline<lex) >0){ seen[$1]=1; } }
    {for(n=1; n<=NF;n++) {  if (seen[$n]) { printf("%s ", $n); } else {printf("<UNK> ",$n);} } printf("\n");}' \
    > $cleantext || exit 1;
 
 
- cat $cleantext | awk '{for(n=2;n<=NF;n++) print $n; }' | sort | uniq -c | \
+cat $cleantext | awk '{for(n=1;n<=NF;n++) print $n; }' | sort | uniq -c | \
     sort -nr > $dir/word.counts || exit 1;
 
 
 # Get counts from acoustic training transcripts, and add  one-count
 # for each word in the lexicon (but not silence, we don't want it
 # in the LM-- we'll add it optionally later).
- cat $cleantext | awk '{for(n=2;n<=NF;n++) print $n; }' | \
+cat $cleantext | awk '{for(n=1;n<=NF;n++) print $n; }' | \
    cat - <(grep -w -v '!SIL' $lexicon | awk '{print $1}') | \
     sort | uniq -c | sort -nr > $dir/unigram.counts || exit 1;
 
-# note: we probably won't really make use of <UNK> as there aren't any OOVs
- cat $dir/unigram.counts  | awk '{print $2}' | get_word_map.pl "<s>" "</s>" "<UNK>" > $dir/word_map \
+cat $dir/unigram.counts  | awk '{print $2}' | get_word_map.pl "<s>" "</s>" "<UNK>" > $dir/word_map \
     || exit 1;
 
 # note: ignore 1st field of train.txt, it's the utterance-id.
- cat $cleantext | awk -v wmap=$dir/word_map 'BEGIN{while((getline<wmap)>0)map[$1]=$2;}
+cat $cleantext | awk -v wmap=$dir/word_map 'BEGIN{while((getline<wmap)>0)map[$1]=$2;}
    { for(n=2;n<=NF;n++) { printf map[$n]; if(n<NF){ printf " "; } else { print ""; }}}' | gzip -c >$dir/train.gz \
     || exit 1;
 
- train_lm.sh --arpa --lmtype ${ngram_order}gram-mincount $dir || exit 1;
+cat $dir/word_map | awk '{print $1}' | cat - <(echo "<s>"; echo "</s>" ) \
+	> $dir/wordlist
 
+ngram-count -text $dir/text.no_oov -order $ngram_order -limit-vocab -vocab $dir/wordlist -unk \
+   -map-unk "<UNK>" -kndiscount -interpolate -lm $dir/srilm.o${ngram_order}g.kn.gz
+
+ngram -lm $dir/srilm.o${ngram_order}g.kn.gz -ppl $heldout > $dir/ppl
 # note: output is
 # $dir/${ngram_order}gram-mincount/lm_unpruned.gz
 echo train lm succeeded
--- egs/gale_tdt_mandarin/s5/run.sh
+++ egs/gale_tdt_mandarin/s5/run.sh
@@ -6,7 +6,7 @@
 
 num_jobs=64
 num_decode_jobs=40
-
+stage=1
 
 GALE_AUDIO=(
   /export/corpora/LDC/LDC2013S08/
@@ -38,14 +38,61 @@ TDT_TEXT=(
   /export/corpora5/LDC/LDC2005T16/
 )
 
+GIGA_TEXT=/export/
+
 galeData=GALE/
 tdtData=TDT/
 
 set -e -o pipefail
 set -x
 
-#local/gale_data_prep_audio.sh "${GALE_AUDIO[@]}" $galeData
-#local/gale_data_prep_txt.sh  "${GALE_TEXT[@]}" $galeData
+if [ $stage -le 0 ]; then
+  echo "Prepare data for GALE"
+  local/gale_data_prep_audio.sh "${GALE_AUDIO[@]}" $galeData
+  local/gale_data_prep_txt.sh  "${GALE_TEXT[@]}" $galeData
+  local/gale_data_prep_split.sh $galeData data/local/gale
 
-#local/tdt_mandarin_data_prep_audio.sh "${TDT_AUDIO[@]}" $tdtData
-local/tdt_mandarin_data_prep_txt.sh  "${TDT_TEXT[@]}" $tdtData
+  echo "Prepare data for TDT"
+  local/tdt_mandarin_data_prep_audio.sh "${TDT_AUDIO[@]}" $tdtData
+  local/tdt_mandarin_data_prep_txt.sh  "${TDT_TEXT[@]}" $tdtData
+  local/tdt_mandarin_data_prep_filter.sh $tdtData data/local/tdt_mandarin
+
+  # Merge transcripts from GALE and TDT for lexicon and LM training
+  mkdir -p data/local/gale_tdt_train
+  cat data/local/gale/train/text data/local/tdt_mandarin/text > data/local/gale_tdt_train/text
+fi
+
+if [ $stage -le 1 ]; then
+  echo "Prepare dictionary for GALE and TDT"
+  local/mandarin_prepare_dict.sh data/local/dict_gale_tdt data/local/gale_tdt_train
+fi
+
+if [ $stage -le 2 ]; then
+  echo "Creating LM for GALE"
+  local/mandarin_prepare_lm.sh --ngram-order 4 --oov-sym "<UNK>" \
+    data/local/dict_gale_tdt data/local/gale/train data/local/gale/train/\lm data/local/gale/dev
+fi
+
+if [ $stage -le 3 ]; then
+  echo "Train a GMM-HMM model with part of GALE data for cleaning up model"
+fi
+
+if [ $stage -le 4 ]; then
+  echo "Clean up TDT data"
+fi
+
+if [ $stage -le 5 ]; then
+  echo "Expand the lexicon with Gigaword"
+  local/gigaword_prepare.sh $GIGA_TEXT
+  local/mandarin_prepare_dict.sh data/local/dict_giga_man_simp data/local/giga_man_simp
+  local/mandarin_merge_dict.sh data/local/dict_gale_tdt data/local/dict_giga_man_simp data/local/dict_gale_tdt_giga
+fi
+
+
+if [ $stage -le 5 ]; then
+  echo "Prepare LM with all data"
+  local/mandarin_prepare_lm.sh --ngram-order 4 --oov-sym "<UNK>" \
+  data/train_cleanup 
+
+
+
