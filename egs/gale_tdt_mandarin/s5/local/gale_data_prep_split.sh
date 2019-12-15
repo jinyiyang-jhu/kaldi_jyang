cc6a5d57e6a8e260851017e504f5dea6d507eb57
Jinyi Yang <jyang126@jhu.edu>
2019-12-14 21:49:44 -0500

    Filter out the transcriptions where multiple speaker speak at the same time.

--- egs/gale_tdt_mandarin/s5/local/gale_data_prep_split.sh
+++ egs/gale_tdt_mandarin/s5/local/gale_data_prep_split.sh
@@ -19,6 +19,11 @@ dir=$(utils/make_absolute.sh $2)
 cat $galeData/all | awk '{print$2}' | \
   sort | uniq -c | awk '{if($1!="1")print$2}' > $galeData/dup.list
 
+# same time duration but different transcription (multiple speaker speaks at same time)
+cat $galeData/all | awk '{print $1" "$3" "$4}' | \
+	sort | uniq -c | awk '{if($1!="1")print $2" "$3" "$4}' > $galeData/dup.segment
+awk 'NR==FNR{a[$1$2$3];next} $1$3$4 in a {print $2}' $galeData/dup.segment $galeData/all >> $galeData/dup.list
+
 utils/filter_scp.pl --exclude -f 2 \
   $galeData/dup.list $galeData/all > $galeData/all.nodup
 
@@ -28,6 +33,13 @@ mv $galeData/all.nodup $galeData/all
 grep    -f <(cat local/gale_dev/test.LDC*) $galeData/all | grep -v -F -f local/gale_bad_utts  > $galeData/all.dev
 
 grep    -f <(cat local/gale_eval/test.LDC*) $galeData/all | grep -v -F -f local/gale_bad_utts  > $galeData/all.eval
+
+# Only parts of the eval transcriptions will be used. We select them from the given segmentation information
+mv $galeData/all.eval $galeData/all.eval.tmp
+cat local/gale_eval/test.*.segment > $galeData/eval.segments.dur
+awk 'NR==FNR{a[$1$2$3];next} $1$3$4 in a {print $0}' $galeData/eval.segments.dur $galeData/all.eval.tmp \
+	> $galeData/all.eval
+rm $galeData/all.eval.tmp
 
 grep -v -f <(cat local/gale_dev/test.LDC*) $galeData/all |\
   grep -v -f <(cat local/gale_eval/test.LDC*) |\
@@ -56,6 +68,15 @@ for x in dev eval train; do
  awk '{print $2 " " $1 " " $3 " " $4}' $file  | sort -u > $outdir/segments
  awk '{printf $2 " "; for (i=5; i<=NF; i++) {printf $i " "} printf "\n"}' $file | sort -u > $outdir/text
 done
+
+#cat local/gale_eval/test.*.segment > $dir/eval/segments.dur
+#mv $dir/eval/segments $dir/eval/segments.tmp
+#awk 'NR==FNR{a[$1$2$3];next} $2$3$4 in a {print $0}' $dir/eval/segments.dur $dir/eval/segments.tmp > $dir/eval/segments
+#rm $dir/eval/segments.tmp
+
+#mv $dir/eval/text $dir/eval/text.tmp
+#awk 'NR==FNR{a[$1];next} $1 in a{print $0}' $dir/eval/segments $dir/eval/text.tmp > $dir/eval/text
+#rm $dir/eval/text.tmp
 
 cat $dir/dev/segments | awk '{print$2}' | sort -u > $galeData/dev.wav.list
 cat $dir/eval/segments | awk '{print$2}' | sort -u > $galeData/eval.wav.list
