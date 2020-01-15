2092a7e8cbf71a0b361e3147d6914fd023741350
Jinyi Yang <jyang126@jhu.edu>
2020-01-15 11:32:59 -0500

    Add comments

--- egs/gale_tdt_mandarin/s5/local/check_oov_rate.sh
+++ egs/gale_tdt_mandarin/s5/local/check_oov_rate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This script check the oov rate of given data set
+# Copyright 2019 Johns Hopkins University (author: Jinyi Yang)
+# This script checks the Out Of Vocabulary words rate of given data set.
+
 
 if [ $# -ne 2 ]; then
   echo "Usage: $0 <lexicon> <test-file>"
@@ -12,7 +14,17 @@ lex=$1
 fname=$2
 
 
-cat $fname | awk '{for(n=2;n<=NF;n++) { print $n; }}' | \
- perl -e '$lex = shift @ARGV; open(L, "<$lex")||die; while(<L>){ @A=split; $seen{$A[0]}=1;}
-   while(<STDIN>) { @A=split; $word=$A[0]; $tot++; if(defined $seen{$word}) {$invoc++; } else {print "OOV word $word\n";}}
-   $oov_rate = 100.0 * (1.0 - ($invoc / $tot)); printf("Seen $invoc out of $tot tokens; OOV rate is %.2f\n", $oov_rate); i' $lex
+cat $fname | awk '{for(n=2;n<=NF;n++) { print $n; }}' | perl -e '
+  $lex = shift @ARGV; open(L, "<$lex")||die;
+  while(<L>){ @A=split; $seen{$A[0]}=1;}
+  while(<STDIN>) {
+    @A=split;
+    $word=$A[0];
+    $tot++;
+    if(defined $seen{$word}) {
+      $invoc++;
+    } else {print "OOV word $word\n";}
+  }
+  $oov_rate = 100.0 * (1.0 - ($invoc / $tot));
+  printf("Seen $invoc out of $tot tokens; OOV rate is %.2f\n", $oov_rate);
+  ' $lex
