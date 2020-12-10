21aee311da2fe0213a85ec7c6efbe9f5c1705a6f
Jinyi Yang <jyang126@jhu.edu>
2020-12-09 23:37:32 -0500

    Fix bug: callhome speech was not listed in the total sph list

--- egs/fisher_callhome_spanish/s5_espnet_data/local/prepare_data_from_espnet/prepare_data.sh
+++ egs/fisher_callhome_spanish/s5_espnet_data/local/prepare_data_from_espnet/prepare_data.sh
@@ -7,12 +7,13 @@ stage=-1
 #espnet_data=/export/fs03/b02/jyang/espnet/egs/fisher_callhome_spanish/st1/data
 espnet_data=./espnet_data
 output_dir=data
-fisher_speech=data/fisher_sph.list
+fisher_callhome_speech=data/sph.list
 fisher_trans=
 callhome_speech=
 callhome_trans=
 sph2pipe="/export/b07/jyang/kaldi-jyang/kaldi/tools/sph2pipe_v2.5/sph2pipe"
-dsets=("fisher_dev" "fisher_dev" "fisher_dev2" "fisher_test" "callhome_devtest" "callhome_evltest")
+fisher_dsets=("fisher_dev" "fisher_dev" "fisher_dev2" "fisher_test")
+callhome_dsets=("callhome_devtest" "callhome_evltest")
 src="es"
 tgt="en"
 
@@ -22,14 +23,11 @@ if [ $stage -le 0 ]; then
     mkdir -p $output_dir/train || exit 1;
     echo "Preparing the original wav.scp for train set"
     espnet_train_dir=$espnet_data/train_sp.$src
-    #cut -d "-" -f2- $espnet_train_dir/wav.scp \
-    #    | awk '{split($0, a, "/"); split(a[9], b, "."); arr[b[1]]=$0} {split($1, c, "-"); (if c[0] in arr) print arr[c[0]]}' $fisher_speech - \
-    #    | awk -F"|" '{print $1"|"$2" |"}' \
-    #    > $output_dir/train/wav.scp
+
     cut -d "-" -f2- $espnet_train_dir/wav.scp | \
          awk -v sph2pipe=$sph2pipe 'NR==FNR{split($0, f1, "/");
             split(f1[9], tmp, "."); all[tmp[1]]=$0; next};
-            {split($1, f2, "-"); if (f2[1] in all){print $1" "sph2pipe" -f wav -p -c "$7" "all[f2[1]] " |"}}' $fisher_speech - | \
+            {split($1, f2, "-"); if (f2[1] in all){print $1" "sph2pipe" -f wav -p -c "$7" "all[f2[1]] " |"}}' $fisher_callhome_speech - | \
             sort -u > $output_dir/train/wav.scp
 
     # Select text
@@ -46,7 +44,24 @@ if [ $stage -le 0 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-    for dset in "${dsets[@]}"; do
+    for dset in "${callhome_dsets[@]}"; do
+        echo "Preparing the $dset"
+        mkdir -p $output_dir/$dset || exit 1;
+        espnet_dset_dir=$espnet_data/${dset}.$src
+        # prepare wav.scp
+        awk -v sph2pipe=$sph2pipe 'NR==FNR{split($0, f1, "/");
+            split(f1[8], tmp, "."); all[tmp[1]]=$0; next};
+            {split($1, f2, "-"); if (f2[1] in all){print $1" "sph2pipe" -f wav -p -c "$7" "all[f2[1]] " |"}}' $fisher_callhome_speech $espnet_dset_dir/wav.scp \
+            > $output_dir/$dset/wav.scp
+        # prepare text
+        sed "s/ &apos; /'/g" $espnet_dset_dir/text.lc.rm > $output_dir/$dset/text || exit 1;
+
+        for f in utt2spk spk2utt utt2num_frames segments spk2gender reco2file_and_channel; do
+            cp $espnet_dset_dir/$f $output_dir/$dset/$f || exit 1;
+        done
+    done
+
+    for dset in "${fisher_dsets[@]}"; do
         echo "Preparing the $dset"
         mkdir -p $output_dir/$dset || exit 1;
         espnet_dset_dir=$espnet_data/${dset}.$src
@@ -54,7 +69,7 @@ if [ $stage -le 2 ]; then
         # prepare wav.scp
         awk -v sph2pipe=$sph2pipe 'NR==FNR{split($0, f1, "/");
             split(f1[9], tmp, "."); all[tmp[1]]=$0; next};
-            {split($1, f2, "-"); if (f2[1] in all){print $1" "sph2pipe" -f wav -p -c "$7" "all[f2[1]] " |"}}' $fisher_speech $espnet_dset_dir/wav.scp \
+            {split($1, f2, "-"); if (f2[1] in all){print $1" "sph2pipe" -f wav -p -c "$7" "all[f2[1]] " |"}}' $fisher_callhome_speech $espnet_dset_dir/wav.scp \
             > $output_dir/$dset/wav.scp
         # prepare text
         sed "s/ &apos; /'/g" $espnet_dset_dir/text.lc.rm > $output_dir/$dset/text || exit 1;
